51bcba6330b1bb4d0199d4ce01c244a8fccb5c95
Alex Diaz <alex@akdev.xyz>
2026-01-31 14:39:46 -0500

    chore: add deploy-alpha job

--- src/landing-page.ts
+++ src/landing-page.ts
@@ -5,10 +5,14 @@ import * as akdev1l from '@akdev1l/constructs';
 
 export class LandingPageStack extends Stack {
   readonly githubProvider: akdev1l.GithubAccessProvider;
+  readonly spa: akdev1l.SinglePageApplication;
 
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
     this.githubProvider = new akdev1l.GithubAccessProvider(this, 'GithubAccess', { repo: "akdev1l/cdk-landing-page-fyi:*" });
+    this.spa = new akdev1l.SinglePageApplication(this, 'LandingPageApplication', {
+      package: '@akdev1l/landing-page',
+    });
   }
 }
